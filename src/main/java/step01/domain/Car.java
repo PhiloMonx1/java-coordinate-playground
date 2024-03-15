4ae384c6bdefc928fe71ca157e88a2ff2f07bd41
LEE TAE MIN <philomonx1@gmail.com>
2024-03-15 20:27:23 +0900

    Feat(연료주입) : 요구 사항 2단계 구현 완료

---
+++ src/main/java/step01/domain/Car.java
@@ -0,0 +1,8 @@
+package step01.domain;
+
+public interface Car {
+	double getDistancePerLiter();
+	double getTripDistance();
+	String getName();
+	double getChargeQuantity();
+}
---
+++ src/main/java/step01/domain/K5.java
@@ -0,0 +1,30 @@
+package step01.domain;
+
+public class K5 implements Car {
+	private static final double DISTANCE_PER_LITER = 13;
+	private final double tripDistance;
+
+	public K5(int tripDistance) {
+		this.tripDistance = tripDistance;
+	}
+
+	@Override
+	public double getDistancePerLiter() {
+		return DISTANCE_PER_LITER;
+	}
+
+	@Override
+	public double getTripDistance() {
+		return tripDistance;
+	}
+
+	@Override
+	public String getName() {
+		return this.getClass().getSimpleName();
+	}
+
+	@Override
+	public double getChargeQuantity() {
+		return getTripDistance() / getDistancePerLiter();
+	}
+}
