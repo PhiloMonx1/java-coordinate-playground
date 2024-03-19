873c2a18f4d7e5e678039ad6c86e6be1e29454ad
LEE TAE MIN <philomonx1@gmail.com>
2024-03-19 17:33:29 +0900

    Refactor(좌표계산기_사각형넓이) : 애플리케이션 `CoordinateView` 사용해서 결과 노출하도록 개선 *refactor : `Square` 클래스 String input 용 생성자 추가

--- src/main/java/step03/CoordinateApplication.java
+++ src/main/java/step03/CoordinateApplication.java
@@ -2,16 +2,26 @@ package step03;
 
 import java.util.Scanner;
 import step03.domain.Line;
+import step03.domain.Square;
+import step03.view.CoordinateView;
 
 public class CoordinateApplication {
 
 	private static final String ASK_COORDINATE = "좌표를 입력하세요.";
-	private static final String REPORT_RESULT = "두 점 사이 거리는 ";
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println(ASK_COORDINATE);
 		String input = scanner.nextLine();
-		System.out.println(REPORT_RESULT + new Line(input).length());
+
+		CoordinateView view = new CoordinateView();
+
+		if (input.split("-").length == 2) {
+			view.printResult(new Line(input));
+		}
+
+		if (input.split("-").length == 4) {
+			view.printResult(new Square(input));
+		}
 	}
 }
