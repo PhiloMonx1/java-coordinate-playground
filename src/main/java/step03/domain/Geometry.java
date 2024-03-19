fcdf9caf1abb5d6cee4d148cb2aa1a8476b42660
LEE TAE MIN <philomonx1@gmail.com>
2024-03-19 17:48:23 +0900

    Refactor(좌표계산기_사각형넓이) : `Geometry::crate()` 내부에서 반복해서 사용하던 split 구문 변수 처리

--- src/main/java/step03/domain/Geometry.java
+++ src/main/java/step03/domain/Geometry.java
@@ -3,12 +3,14 @@ package step03.domain;
 public abstract class Geometry {
 
 	public static Geometry crate(String input) {
-		if (input.split("-").length == 2) {
-			return new Line(new Point(input.split("-")[0]), new Point(input.split("-")[1]));
+		String[] splitInput = input.split("-");
+
+		if (splitInput.length == 2) {
+			return new Line(new Point(splitInput[0]), new Point(splitInput[1]));
 		}
-		if (input.split("-").length == 4) {
-			return new Square(new Point(input.split("-")[0]), new Point(input.split("-")[1]),
-					new Point(input.split("-")[2]), new Point(input.split("-")[3]));
+		if (splitInput.length == 4) {
+			return new Square(new Point(splitInput[0]), new Point(splitInput[1]),
+					new Point(splitInput[2]), new Point(splitInput[3]));
 		}
 		throw new IllegalArgumentException("좌표가 잘못되었습니다.");
 	}
