2b271e9342c26dcab42fcc8facdba7059606d340
LEE TAE MIN <philomonx1@gmail.com>
2024-03-20 15:49:58 +0900

    Refactor(좌표계산기_피드백) : `FigureFactory::getInstance()` 내부 구현 로직 `FigureCreator::create()`로 구현하도록 개선

--- src/main/java/exercise_polymorphism/FigureCreator.java
+++ src/main/java/exercise_polymorphism/FigureCreator.java
@@ -3,5 +3,20 @@ package exercise_polymorphism;
 import java.util.List;
 
 public interface FigureCreator {
-    Figure create(List<Point> points);
+
+	static Figure create(List<Point> points) {
+		if (points.size() == Line.LINE_POINT_SIZE) {
+			return new Line(points);
+		}
+
+		if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
+			return new Triangle(points);
+		}
+
+		if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
+			return new Rectangle(points);
+		}
+
+		throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+	}
 }
