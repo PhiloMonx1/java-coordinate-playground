ab6fa175ab4990cda5e834502123f7c48247a728
LEE TAE MIN <philomonx1@gmail.com>
2024-03-20 16:06:14 +0900

    Feat(좌표계산기_피드백) : `javajigi` 브랜치 Copy

---
+++ src/main/java/javajigi/CoordinateMain.java
@@ -0,0 +1,9 @@
+package javajigi;
+
+import javajigi.controller.CoordinateCalculator;
+
+public class CoordinateMain {
+    public static void main(String[] args) {
+        new CoordinateCalculator().run();
+    }
+}
