188fb6cf396706a8cb1f34bf74676f1f7b6e98cc
LEE TAE MIN <philomonx1@gmail.com>
2024-03-22 19:39:21 +0900

    Feat(좌표계산기_재구현) : 메인, 컨트롤러 구현 (구현 완료) *add : `CoordinateCalculator` 추가 *add : `CoordinateMain` 추가

---
+++ src/main/java/step05/CoordinateMain.java
@@ -0,0 +1,11 @@
+package step05;
+
+import step05.controller.CoordinateCalculator;
+
+public class CoordinateMain {
+
+	public static void main(String[] args) {
+		new CoordinateCalculator().run();
+	}
+
+}
---
+++ src/main/java/step05/controller/CoordinateCalculator.java
@@ -0,0 +1,19 @@
+package step05.controller;
+
+import java.util.List;
+import step05.model.Figure;
+import step05.model.FigureFactory;
+import step05.view.InputView;
+import step05.model.Point;
+import step05.model.PointFactory;
+import step05.view.OutputView;
+
+public class CoordinateCalculator {
+
+	public void run() {
+		List<int[]> coordinates = InputView.inputCoordinates();
+		List<Point> points = PointFactory.create(coordinates);
+		Figure figure = FigureFactory.create(points);
+		OutputView.showCoordinate(figure);
+	}
+}
